4bc833bf1e89acc13126c706f3844ddae78de37a
Gabriel Malosto <professional@gabdumal.com>
2022-10-27 18:07:02 -0300

    Inicia projeto

---
+++ src/main/java/com/lugar/confeitaria/Confeitaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.lugar.confeitaria;
+
+/**
+ *
+ * @author lugar
+ */
+public class Confeitaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
